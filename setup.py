eb5d773e2cf2c1fb693e683ce57d8f906c20dcde
timchen119 <timchen119@gmail.com>
2009-12-29 12:16:30 +0800

    change package format to setuptools and upload to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='g5mouse',
     version='0.1',
-    scripts=['g5mouse.py'],
+    scripts=['bin/g5mouse.py'],
     author='timchen119',
     author_email='timchen119@gmail.com',
     description='A pure python code to control logitech G3,G5,G7,G9 mouse on linux',
     license='New BSD License',
-    url='http://code.google.com/p/g5mouse/')
+    url='http://code.google.com/p/g5mouse/',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
 
